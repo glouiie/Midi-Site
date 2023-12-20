d6eeac851ebae0f1b5ab0a480e3c1d1b29d381a4
Glouiie <louiebeatty2020@gmail.com>
2023-12-20 17:39:14 +0000

    text rendering to screen done

--- main.js
+++ main.js
@@ -7,8 +7,7 @@ import { pianoSounds,TextArray } from './instruments';
 import {initializeKeyboardInput } from './computerKBInput';
 import { loadMidiFile } from './midiFileHandler';
 
-
-// put this in seperate file
+// put this in seperate file or dont idc
 document.getElementById('loadSmallPiano').addEventListener('click', function() {
     loadPianoModel('smaller_piano.gltf');
 });
@@ -33,8 +32,7 @@ initializeKeyboardInput(playSound, getInstrumentNumber);
 let instrumentNumber = 0;
 const midiHandler = new MidiHandler(instrumentNumber, playSound);
 
-
-
+//scene setup
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -55,48 +53,63 @@ orbit.enablePan = false;
 orbit.update();
 
 scene.background = new THREE.Color(0xC5FFF8);
-
-
-//adding text
-//TODO ONLY ON ONE SURFACE OR WHATNOT SO DIFFERENT NOT STATIONARY!
-
-const labelRenderer = new CSS2DRenderer()
-labelRenderer.setSize(window.innerWidth, window.innerHeight)
-
-labelRenderer.domElement.style.position = "absolute"
-labelRenderer.domElement.style.top = "0px"
-labelRenderer.domElement.style.pointerEvents = "none"
-document.body.appendChild(labelRenderer.domElement)
-
-let keyboardText = document.createElement("p")
-keyboardText.textContent = TextArray[instrumentNumber];
-keyboardText.style.fontSize = '30px';
-keyboardText.style.fontFamily = "comic Sans MS"
-const cPointLabel = new CSS2DObject(keyboardText)
-scene.add(cPointLabel);
-cPointLabel.position.set(0,0,0)
-
-
 var light = new THREE.HemisphereLight(0xffffff,0.5);
 scene.add(light);
 
-let screenObject = null;
+function animate() {
+    renderer.render(scene, camera);
+}
+renderer.setAnimationLoop(animate);
+
+//adding text to screen
+let currentPianoModel =" "
+function updateInstrumentLabel(instrumentName) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width = 850; 
+    canvas.height = 300;
+    //flip the text upside down otherwise it doesnt work
+    context.translate(0, canvas.height);
+    context.scale(1, -1);
+  
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.font = `150px Comic Sans MS`;
+    context.fillStyle = 'black';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+
+    context.fillText(instrumentName, canvas.width / 2, canvas.height / 2);
+
+    const texture = new THREE.Texture(canvas);
+    texture.needsUpdate = true; 
+    texture.minFilter = THREE.LinearFilter; 
+
+    scene.traverse((object) => {
+        if (object.material && object.material.name === 'Front Face') {
+            object.material.map = texture;
+            object.material.transparent = true;
+            object.material.needsUpdate = true;
+        }
+    });
+}
 
 let noteToObjectMap = {};
-
-
 let Selector_Left = null
-
 let Selector_Right = null
 
 const loader = new GLTFLoader();
 function loadPianoModel(filename) {
+
+    currentPianoModel = filename
+
     loader.load(`assets/${filename}`, (gltf) => {
         if (scene.children.some(child => child.name === "PianoModel")) {
             const existingModel = scene.children.find(child => child.name === "PianoModel");
             scene.remove(existingModel);
         }
-
         const model = gltf.scene;
         model.name = "PianoModel";
         model.position.set(0, 0, 0);
@@ -104,62 +117,38 @@ function loadPianoModel(filename) {
         model.scale.set(1, 1, 1);
         scene.add(model);
 
-        noteToObjectMap = {};
-
         model.traverse((child) => {
             if (child.name.startsWith("Key")) {
                 const note = child.name.split('_')[1];
                 noteToObjectMap[note] = child; 
             }
             
-            if (child.name === "Screen") {
-                screenObject = child;
-            }
             if (child.name.startsWith("Selector_Left")) {
                 Selector_Left = child; 
             }
             if (child.name.startsWith("Selector_Right")) {
                 Selector_Right = child; 
             }
+
         });
 
         highlightedObjects.forEach(obj => resetHighlightedObjects(obj));
         highlightedObjects = [];
-
-        keyboardText.textContent = TextArray[instrumentNumber];
+        updateInstrumentLabel(TextArray[instrumentNumber])
     });
+
 }
 loadPianoModel("bigger_piano.gltf");
-
 //smaller_piano.gltf
 //bigger_piano.gltf
-function animate() {
-    if (screenObject && cPointLabel) {
-        cPointLabel.position.set(0.1000000,0.45154336750507355,-0.3542221546173096);
-    }
-    renderer.render(scene, camera);
-    labelRenderer.render(scene, camera);
 
-}
-
-renderer.setAnimationLoop(animate);
 
 window.addEventListener('resize', function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    labelRenderer.setSize(this.window.innerWidth, this.window.innerHeight);
-}
-);
-
-function playSound(noteName, instNumber) {
-    const keyObject = noteToObjectMap[noteName];
-    if (keyObject) {
-        highlightObject(keyObject, blueColour);
     }
-    pianoSounds[instNumber].playSound(noteName);
-    
-}
+);
 
 function getPointerPosition(event) {
     const canvasBounds = renderer.domElement.getBoundingClientRect();
@@ -189,16 +178,22 @@ function highlightObject(object, color) {
         }
     }
 }
-
+const blueColour =  0x0000FF
 function resetHighlightedObjects(object) {
     if (object && originalMaterials.has(object)) {
         object.material = originalMaterials.get(object);
         originalMaterials.delete(object);
     }
 }
+//music stuff
+function playSound(noteName, instNumber) {
+    const keyObject = noteToObjectMap[noteName];
+    if (keyObject) {
+        highlightObject(keyObject, blueColour);
+    }
+    pianoSounds[instNumber].playSound(noteName);
+}
 
-
-//TODO MAKE + /- CHANGE INSTRUMENT IN OTHER FILE
 function handleInstrumentChange(direction) {
     instrumentNumber += direction;
     const totalInstruments = pianoSounds.length;
@@ -207,27 +202,11 @@ function handleInstrumentChange(direction) {
     } else if (instrumentNumber >= totalInstruments) {
         instrumentNumber = 0;
     }
-    keyboardText.textContent = TextArray[instrumentNumber];
+    updateInstrumentLabel(TextArray[instrumentNumber])
 }
-
-const blueColour =  0x0000FF
-
-function getRelevantObjectsForRaycasting() {
-    let relevantObjects = Object.values(noteToObjectMap).filter(obj => obj.name.startsWith("Key"));
-    if (Selector_Left) relevantObjects.push(Selector_Left);
-    if (Selector_Right) relevantObjects.push(Selector_Right);
-    return relevantObjects;
-}
-
-const raycastThrottleInterval = 40; 
-
-let raycastThrottleTimeout = null;
-
 
 function objectSelect(event) {
-
     if (raycastThrottleTimeout) return; 
-
     raycastThrottleTimeout = setTimeout(() => {
         performRaycasting(event); 
         raycastThrottleTimeout = null; 
@@ -235,12 +214,19 @@ function objectSelect(event) {
 }
 
 
-
 //mouse hover
+
+function getRelevantObjectsForRaycasting() {
+    let relevantObjects = Object.values(noteToObjectMap).filter(obj => obj.name.startsWith("Key"));
+    if (Selector_Left) relevantObjects.push(Selector_Left);
+    if (Selector_Right) relevantObjects.push(Selector_Right);
+    return relevantObjects;
+}
+const raycastThrottleInterval = 40; 
+let raycastThrottleTimeout = null;
 let currentlyHoveredObjects = new Set();
 
 function performRaycasting(event) {
-
 
     let actionPerformedForLeftSelector = false;
     let actionPerformedForRightSelector = false;
@@ -256,15 +242,12 @@ function performRaycasting(event) {
     let newlyHoveredObjects = new Set();
     
     if (intersects.length > 0) {
-
-
         intersects.forEach(intersect => {
             const intersectedObject = intersect.object;
 
             if (intersectedObject.name.startsWith("Selector_Left")) {
                 newlyHoveredObjects.add(intersectedObject);
                 if (!currentlyHoveredObjects.has(intersectedObject) && !actionPerformedForLeftSelector) {
-                    console.log("Hi from Left Selector");
                     handleInstrumentChange(-1);
                     highlightObject(intersectedObject, blueColour);
                     actionPerformedForLeftSelector = true;
@@ -273,14 +256,11 @@ function performRaycasting(event) {
             else if (intersectedObject.name.startsWith("Selector_Right")) {
                 newlyHoveredObjects.add(intersectedObject);
                 if (!currentlyHoveredObjects.has(intersectedObject) && !actionPerformedForRightSelector) {
-                    console.log("Hi from Right Selector");
                     handleInstrumentChange(1);
                     highlightObject(intersectedObject, blueColour);
                     actionPerformedForRightSelector = true;
                 }
             }
-
-
             if (intersectedObject.name.startsWith("Key")) {
                 let noteName = intersectedObject.name.split('_')[1];
                 newlyHoveredObjects.add(intersectedObject);
@@ -301,8 +281,12 @@ function performRaycasting(event) {
     currentlyHoveredObjects = new Set(newlyHoveredObjects);
 }
 
+
+
+
+
 window.addEventListener("mousemove", objectSelect);
 
 
 
-export{resetHighlightedObjects,noteToObjectMap,handleInstrumentChange}
+export{resetHighlightedObjects,noteToObjectMap,handleInstrumentChange,playSound, getInstrumentNumber}
